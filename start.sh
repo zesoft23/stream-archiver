67c7bd04b28cf1ce70ac35aabc5edea22ab39aa9
Jack T <thomsonjack41@protonmail.com>
2021-04-14 15:14:10 -0400

    Add sed command to enable browsing folders

--- start.sh
+++ start.sh
@@ -2,4 +2,5 @@
 
 chmod a+w /dev/pts/0
 python3 /home/archiver.py &
+sed -i 's/#   dir-listing.activate      = "enable"/dir-listing.activate = "enable"/g' /etc/lighttpd/lighttpd.conf 
 exec lighttpd -D -f /etc/lighttpd/lighttpd.conf
